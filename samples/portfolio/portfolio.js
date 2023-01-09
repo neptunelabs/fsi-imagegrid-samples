8cd4310653748c595e627f3d79ccebdfae95ac53
biernat <biernat@neptunelabs.com>
2023-01-09 10:51:47 +0100

    added framework

--- samples/portfolio/portfolio.js
+++ samples/portfolio/portfolio.js
@@ -1,4 +1,4 @@
 function openPopUp(evt, oCell){
-  $('#modalSelection').modal('toggle');
+  $('#modalSelection').modal('show');
   document.getElementById('fsi-grid-fit').assignFSIViewer('grid-viewer');
 }
