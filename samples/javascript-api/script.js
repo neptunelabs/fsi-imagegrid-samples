3f6f1ef9267b3ff9a089024d13e04d8b77cd1ad9
kranzhoff <kranzhoff@neptunelabs.com>
2023-12-05 14:17:00 +0100

    - fixed api sample

--- samples/javascript-api/script.js
+++ samples/javascript-api/script.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
   document.getElementById('myViewer').addListener('onInitFailed', () => {
     document.getElementById('gridBtn').disabled = false;
-    document.getElementById("myModal").removeAttribute("style");
+    //document.getElementById("myModal").removeAttribute("style");
   });
 
   document.getElementById('gridBtn').addEventListener('click', () => {
@@ -17,13 +17,17 @@ document.addEventListener('DOMContentLoaded', () => {
       document.getElementById('productContainer').style.height = '600px'
       document.getElementById('gridViewer').style.height = '600px'
       document.getElementById('gridText').style.display = 'none'
-      addMetadata()
+      var mm = document.getElementById('myModal');
+      mm.style.visibility = "visible";
+      mm.style.display = "none";
+
     }
 
     const instance = $FSI.createNode("fsi-imagegrid",  {
         imagesources: 'images/samples/ssi/furniture/home-7473734.jpg,images/samples/ssi/furniture/home-7531451.jpg,images/samples/ssi/furniture/home-7531461_1920.jpg, images/samples/ssi/furniture/home-7531469.jpg, images/samples/ssi/furniture/home-7567164.jpg, images/samples/ssi/furniture/interior-design-6012873.jpg, images/samples/ssi/furniture/dresser-6717656.jpg, images/samples/ssi/furniture/living-room-7225005.jpg,images/samples/ssi/furniture/living-room-7547558.jpg,images/samples/ssi/furniture/home-2082923.jpg',
         debug: true,
         width:'1500px',
+        height:'800px',
         cellWidth: '250',
         cellHeight: '250',
         preloadCount: '120',
@@ -32,16 +36,16 @@ document.addEventListener('DOMContentLoaded', () => {
         viewerSelector:'#myViewer',
         // listen for finished loading FSI ImageGrid and becomes interactive
         onReady: show
-      }, true)
+      });
 
       instance.setAttribute("data-bs-target","#myModal")
       instance.setAttribute("data-bs-toggle","modal")
 
+      addMetadata(instance);
       document.getElementById('gridEle').appendChild(instance)
 
         // add metadata stuff
-    function addMetadata () {
-     console.log('Metadata here?')
+    function addMetadata (instance) {
      const tpl = document.createElement ("fsi-imagegrid-template");
       tpl.innerHTML = "      <div class=\"myImageGridTitle\">\n" +
        "          ###iptc.Headline###\n" +
@@ -54,7 +58,7 @@ document.addEventListener('DOMContentLoaded', () => {
        "      ###iptc.Caption### <br/>###iptc.FSI Extra###\n" +
        "        </span>\n" +
        "      </div>";
-      document.getElementById('gridViewer').appendChild(tpl)
+      instance.appendChild(tpl)
    }
 
   })
