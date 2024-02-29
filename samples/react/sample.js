24beada219b7fbb84b2642d33ae7683632d9f85c
nelabiernat <60930527+nelabiernat@users.noreply.github.com>
2024-02-29 15:16:40 +0100

    changed react sample

--- samples/react/sample.js
+++ samples/react/sample.js
@@ -1,10 +1,12 @@
 const Sample = () => {
   return (
     <div className='fsi-viewer-section'>
-      <FSIComponent tagName="fsi-imagegrid" width={"100%"} height={"330px"}
-                    dir={"images/samples/imagegrid/landscape"}
-                    cellWidth={200} cellHeight={100} preloadCount={60}
+    <div className="container py-4">
+      <FSIComponent tagName="fsi-imagegrid" width={"1500px"} height={"600px"}
+                    imagesources={"images/samples/ssi/furniture/home-7473734.jpg,images/samples/ssi/furniture/home-7531451.jpg,images/samples/ssi/furniture/home-7531461_1920.jpg, images/samples/ssi/furniture/home-7531469.jpg, images/samples/ssi/furniture/home-7567164.jpg, images/samples/ssi/furniture/interior-design-6012873.jpg, images/samples/ssi/furniture/dresser-6717656.jpg, images/samples/ssi/furniture/living-room-7225005.jpg,images/samples/ssi/furniture/living-room-7547558.jpg,images/samples/ssi/furniture/furniture-6048139.jpg"}
+                    cellWidth={250} cellHeight={290} preloadCount={60}
                     autoCrop={"cc"} />
+    </div>
     </div>
   );
 };
